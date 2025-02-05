af71cabec3959ff32f0a71fbdfa285aa98191314
Melisa Simsek <melisaxsimsek@gmail.com>
2025-02-05 14:17:16 +0300

    project3

---
+++ Slider projesi/script.js
@@ -0,0 +1,39 @@
+const nextIcon = document.querySelector(".next");
+const imageContainer = document.querySelector(".imageContainer");
+const imgs=document.querySelectorAll('img');
+const prevIcon = document.querySelector(".prev");
+
+let timeout;
+let currentImg = 1;
+
+prevIcon.addEventListener("click", () => {
+  currentImg--;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+
+nextIcon.addEventListener("click", () => {
+  currentImg++;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+function updateImg() {
+  if(currentImg>imgs.length){
+    currentImg=1;
+  }
+  else if(currentImg<1){
+    currentImg=imgs.length;
+  }
+  imageContainer.style.transform = `translateX(-${(currentImg - 1) * 700}px)`;
+  imageContainer.style.transition = "transform 0.5s ease-in-out";
+timeout=setTimeout(()=>{
+  currentImg++;
+  updateImg();
+},3000)
+
+}
+
+
+updateImg();
